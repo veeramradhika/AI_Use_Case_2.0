b8e75975a03d86f808ba355911475e4e9b4521f0
veeramradhika <veeramradhika@gmail.com>
2023-07-25 16:14:15 +0530

    final

--- AI_Use_Case_2.0/Controllers/HomeController.cs
+++ AI_Use_Case_2.0/Controllers/HomeController.cs
@@ -1,18 +1,22 @@
-﻿using AI_Use_Case_2._0.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace AI_Use_Case_2._0.Controllers
+namespace Use_Case_2.Controllers
 {
     public class HomeController : Controller
     {
-        const string API_KEY = "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";
+        const string API_KEY = "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";
         private readonly ILogger<HomeController> _logger;
         static readonly HttpClient client = new HttpClient
         {
-            Timeout = TimeSpan.FromSeconds(500) // Set a timeout of 300 seconds (5 minutes)
+            Timeout = TimeSpan.FromSeconds(500) // Set a timeout of 500 seconds (5 minutes)
         };
 
         public HomeController(ILogger<HomeController> logger)
@@ -26,76 +30,83 @@ namespace AI_Use_Case_2._0.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Get(string prompt1, string prompt2, string prompt3, string csvFileName1, string csvFileName2, string csvFileName3)
+        public async Task<IActionResult> Get(bool useFileAsReference, string prompt, IFormFile file, string filename)
         {
-            var prompts = new string[] { prompt1, prompt2, prompt3 };
-            var resultFiles = new List<string>();
-            var inputFiles = new List<string>();
+            string inputPrompt = prompt;
+
+            if (useFileAsReference && file != null)
+            {
+                try
+                {
+                    // Read and parse the CSV file
+                    using (var reader = new StreamReader(file.OpenReadStream()))
+                    {
+                        List<string[]> rows = new List<string[]>();
+                        while (!reader.EndOfStream)
+                        {
+                            string line = await reader.ReadLineAsync();
+                            string[] data = line.Split(',');
+                            rows.Add(data);
+                        }
+
+                        // Filter the data you want to include in the prompt from the CSV file
+                        // For example, you can join specific columns or rows to create the input prompt
+                        List<string> filteredData = rows.Select(row => string.Join(", ", row)).ToList();
+
+                        // Combine the filtered data and the user-provided prompt to create the final input prompt
+                        inputPrompt = string.Join("\n", filteredData) + "\n\n" + prompt;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return Json("Error reading the CSV file: " + ex.Message);
+                }
+            }
+
+            if (string.IsNullOrEmpty(inputPrompt))
+            {
+                return Json("Please provide a prompt.");
+            }
 
             var options = new Dictionary<string, object>
             {
                 { "model", "gpt-3.5-turbo" },
                 { "max_tokens", 3500 },
-                { "temperature", 0.2 }
+                { "temperature", 0.1 }
             };
 
             client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", API_KEY);
 
             try
             {
-                for (int i = 0; i < prompts.Length; i++)
+                options["messages"] = new[]
                 {
-                    string input = prompts[i];
-
-                    if (!string.IsNullOrEmpty(input))
+                    new
                     {
-                        options["messages"] = new[]
-                        {
-                            new
-                            {
-                                role = "user",
-                                content = input
-                            }
-                        };
-
-                        var json = JsonConvert.SerializeObject(options);
-                        var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-                        var startTime = DateTime.Now;
-                        var response = await client.PostAsync("https://api.openai.com/v1/chat/completions", content);
-                        response.EnsureSuccessStatusCode();
-
-                        var responseBody = await response.Content.ReadAsStringAsync();
-                        dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                        string result = jsonResponse.choices[0].message.content;
-                        var endTime = DateTime.Now;
-                        var responseTime = (endTime - startTime).TotalMilliseconds;
-
-                        // Save the data to a CSV file with the user-provided file name
-                        string csvContent = $"Prompt: {input}\nGenerated Response: {result}\nResponse Time (ms): {responseTime}";
-                        string fileName = $"result_{i + 1}.csv"; // Default file name if user doesn't provide one
-
-                        // Use the user-provided file name if available
-                        if (i == 0 && !string.IsNullOrEmpty(csvFileName1))
-                            fileName = $"{csvFileName1}.csv";
-                        else if (i == 1 && !string.IsNullOrEmpty(csvFileName2))
-                            fileName = $"{csvFileName2}.csv";
-                        else if (i == 2 && !string.IsNullOrEmpty(csvFileName3))
-                            fileName = $"{csvFileName3}.csv";
-
-                        string filePath = Path.Combine(Path.GetTempPath(), fileName);
-                        await System.IO.File.WriteAllTextAsync(filePath, csvContent, Encoding.UTF8);
-                        resultFiles.Add(fileName);
-
-                        // Store the input files for the third prompt
-                        if (i < 2)
-                        {
-                            inputFiles.Add(filePath);
-                        }
+                        role = "user",
+                        content = inputPrompt
                     }
-                }
+                };
 
-                ViewBag.FileNames = resultFiles;
+                var json = JsonConvert.SerializeObject(options);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var startTime = DateTime.Now;
+                var response = await client.PostAsync("https://api.openai.com/v1/chat/completions", content);
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
+                string result = jsonResponse.choices[0].message.content;
+                var endTime = DateTime.Now;
+                var responseTime = (endTime - startTime).TotalMilliseconds;
+
+                // Save the data to a CSV file with the specified filename
+                string csvContent = $"Generated Response: {result}";
+                string filePath = Path.Combine(Path.GetTempPath(), $"{filename}.csv");
+                await System.IO.File.WriteAllTextAsync(filePath, csvContent, Encoding.UTF8);
+
+                ViewBag.FileNames = new List<string> { $"{filename}.csv" };
                 return View("Index");
             }
             catch (Exception ex)
@@ -104,7 +115,7 @@ namespace AI_Use_Case_2._0.Controllers
             }
         }
 
-        // Add a new action to download the CSV file for a specific prompt
+        // Add a new action to download the CSV file for a specific filename
         public IActionResult Download(string fileName)
         {
             if (!string.IsNullOrEmpty(fileName))
